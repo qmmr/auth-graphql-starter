2980bed90327653c106f00c3bf54739c238fecff
Marcin Kumorek <marcin@kalohq.com>
2018-02-04 21:42:49 +0100

    Add dotenv to store DB user and password

--- server/server.js
+++ server/server.js
@@ -8,11 +8,14 @@ const passportConfig = require('./services/auth');
 const MongoStore = require('connect-mongo')(session);
 const schema = require('./schema/schema');
 
+// init dotenv
+require('dotenv').config()
+
 // Create a new Express application
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://' + process.env.DB_USER + ':' + process.env.DB_PASSWORD + '@ds125288.mlab.com:25288/graphql-auth';
 
 // Mongoose's built in promise library is deprecated, replace it with ES2015 Promise
 mongoose.Promise = global.Promise;
